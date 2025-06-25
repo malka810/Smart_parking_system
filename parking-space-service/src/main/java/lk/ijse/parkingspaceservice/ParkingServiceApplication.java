e2adf8e53c03f36cd127c144f1b8039ce3b997ad
Malka Samarakoon <malkasamarakoon30@gmail.com>
2025-06-25 16:28:36 +0530

    parking space service done

---
+++ parking-space-service/src/main/java/lk/ijse/parkingspaceservice/ParkingServiceApplication.java
@@ -0,0 +1,24 @@
+package lk.ijse.parkingspaceservice;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setAmbiguityIgnored(true);
+        return modelMapper;
+    }
+
+}
